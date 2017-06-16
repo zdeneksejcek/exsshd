677f9e1a38d0d842f48c6ec550cff75955b317b8
Zdenek Sejcek <zdenek@sejcek.cz>
2017-06-16 11:01:20 +0200

    initial commit

---
+++ lib/exsshd.ex
@@ -0,0 +1,18 @@
+defmodule ExSSHd do
+  @moduledoc """
+  Documentation for ExSSHd.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExSSHd.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
